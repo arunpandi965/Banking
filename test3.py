4a882e72eabdf3d204091ba8a93a33e2677dac1b
arunpandi965 <parunpandi.2014@gmail.com>
2023-11-24 11:51:14 +0530

    while with for

--- test3.py
+++ test3.py
@@ -1,3 +1,4 @@
+print("WELLCOME")
 print("(1)AVAILABLE")
 print("(2)DEPOSIT")
 print("(3)WITHDRAW")
@@ -5,23 +6,25 @@ print("(4)EXIT")
 balance = 0
 withdraw = 0
 i = 0
-while i < 5:
-     selectnumber=input("please select your number:")
-     if selectnumber=="1":
-          print("available balance is:",balance)
-     elif selectnumber=="2":
-          deposit=int(input("enter the deposit amount:"))
-          balance=deposit+balance
-          print("available balance is:",balance)          
-     elif selectnumber=="3":
-          withdraw=int(input("enter the amount withdraw:"))
-          if(balance < withdraw):
-               print("you have insufficient balance")
-          else:
-               balance=balance-withdraw
+while i < 2:
+     for x in range(5):
+          selectnumber=input("please select your number:")
+          if selectnumber=="1":
                print("available balance is:",balance)
-     elif selectnumber=="4":
-          print("your transection completed")
-     else:
-          print("thank you")
+          elif selectnumber=="2":
+               deposit=int(input("enter the deposit amount:"))
+               balance=deposit+balance
+               print("available balance is:",balance)          
+          elif selectnumber=="3":
+               withdraw=int(input("enter the amount withdraw:"))
+               if(balance < withdraw):
+                    print("you have insufficient balance")
+               else:
+                    balance=balance-withdraw
+                    print("available balance is:",balance)
+          elif selectnumber=="4":
+               print("your transection completed")
+          else:
+               print("Thank you for Banking")
+
      i +=1
